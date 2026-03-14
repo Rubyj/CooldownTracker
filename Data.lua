4a319e19dec7935fd2b001122a97b3bc0a04ae22
RubyJ <reuben.jacobs1@gmail.com>
2026-03-14 13:43:42 -0400

    Add full healer cooldown roster (Druid, Paladin, Shaman, Warrior, Evoker, DK)

--- Data.lua
+++ Data.lua
@@ -7,14 +7,14 @@
 --   name     - ability name (display only)
 --   duration - cooldown length in seconds
 --   icon     - WoW texture path (Interface\Icons\...)
---   r, g, b  - accent colour (0-1 range)
+--   r, g, b  - accent colour using WoW class colours (0-1 range)
 --------------------------------------------------------------------------------
 
 local AddonName, CT = ...
 
 CT.COOLDOWNS = {
     -- -------------------------------------------------------------------------
-    -- Druid
+    -- Druid (class colour: orange)
     -- -------------------------------------------------------------------------
     {
         id       = "druid_convoke",
@@ -22,7 +22,7 @@ CT.COOLDOWNS = {
         name     = "Convoke the Spirits",
         duration = 60,
         icon     = "Interface\\Icons\\ability_ardenweald_druid",
-        r        = 0.4, g = 0.8, b = 0.4,
+        r        = 1.0, g = 0.49, b = 0.04,
     },
     {
         id       = "druid_tranquility",
@@ -30,6 +30,82 @@ CT.COOLDOWNS = {
         name     = "Tranquility",
         duration = 180,
         icon     = "Interface\\Icons\\spell_nature_tranquility",
-        r        = 0.4, g = 0.6, b = 1.0,
+        r        = 1.0, g = 0.49, b = 0.04,
+    },
+
+    -- -------------------------------------------------------------------------
+    -- Paladin (class colour: pink)
+    -- -------------------------------------------------------------------------
+    {
+        id       = "paladin_avenging_wrath",
+        class    = "Paladin",
+        name     = "Avenging Wrath",
+        duration = 120,
+        icon     = "Interface\\Icons\\spell_holy_avenginewrath",
+        r        = 0.96, g = 0.55, b = 0.73,
+    },
+    {
+        id       = "paladin_aura_mastery",
+        class    = "Paladin",
+        name     = "Aura Mastery",
+        duration = 120,
+        icon     = "Interface\\Icons\\spell_holy_auramastery",
+        r        = 0.96, g = 0.55, b = 0.73,
+    },
+
+    -- -------------------------------------------------------------------------
+    -- Shaman (class colour: blue)
+    -- -------------------------------------------------------------------------
+    {
+        id       = "shaman_ascendance",
+        class    = "Shaman",
+        name     = "Ascendance",
+        duration = 180,
+        icon     = "Interface\\Icons\\spell_fire_elementaldevastation",
+        r        = 0.0, g = 0.44, b = 0.87,
+    },
+    {
+        id       = "shaman_spirit_link",
+        class    = "Shaman",
+        name     = "Spirit Link Totem",
+        duration = 180,
+        icon     = "Interface\\Icons\\spell_shaman_spiritlink",
+        r        = 0.0, g = 0.44, b = 0.87,
+    },
+
+    -- -------------------------------------------------------------------------
+    -- Warrior (class colour: tan/gold)
+    -- -------------------------------------------------------------------------
+    {
+        id       = "warrior_rallying_cry",
+        class    = "Warrior",
+        name     = "Rallying Cry",
+        duration = 180,
+        icon     = "Interface\\Icons\\ability_warrior_rallyingcry",
+        r        = 0.78, g = 0.61, b = 0.23,
+    },
+
+    -- -------------------------------------------------------------------------
+    -- Evoker (class colour: teal)
+    -- -------------------------------------------------------------------------
+    {
+        id       = "evoker_zephyr",
+        class    = "Evoker",
+        name     = "Zephyr",
+        duration = 120,
+        icon     = "Interface\\Icons\\ability_evoker_hoverblack",
+        r        = 0.2, g = 0.58, b = 0.5,
+    },
+
+    -- -------------------------------------------------------------------------
+    -- Death Knight (class colour: red)
+    -- -------------------------------------------------------------------------
+    {
+        id       = "dk_amz",
+        class    = "Death Knight",
+        name     = "Anti-Magic Zone",
+        duration = 120,
+        icon     = "Interface\\Icons\\spell_deathknight_antimagiczone",
+        r        = 0.77, g = 0.12, b = 0.23,
     },
 }
